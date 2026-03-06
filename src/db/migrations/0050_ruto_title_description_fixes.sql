5c8c1a115e368b153f18b63ca47a1c4ec9e1ff39
gavinmcfall <gavin@nerdz.co.nz>
2026-03-06 22:18:39 +1300

    fix: Ruto contract titles to Title Case + clean template variables
    - Title-case all Synced Assassination contract titles (were lowercase)
    - Replace ~mission(TargetName) template vars with "Target A" / "Target B"

---
+++ src/db/migrations/0050_ruto_title_description_fixes.sql
@@ -0,0 +1,43 @@
+-- Fix Ruto Synced Assassination contract titles (lowercase -> Title Case)
+-- and replace ~mission(TargetName) template variables with "Target A" / "Target B"
+UPDATE contracts SET title = 'Together Forever', description = 'Here''s the deal. Target A and Target B need to die at the same time. Unfortunately, they aren''t gonna be in the same place.
+
+A double hit can sound complicated, but if you hire some additional guns it shouldn''t be all that hard to do two hits simultaneously.
+
+Make sure you find folks who have skills as I have it on good authority that each''ll be flying with some escorts for "protection."' WHERE id = 75;
+UPDATE contracts SET title = 'Twice the Headache', description = 'Have another pair that needs simultaneous deathing. Seems to be getting more and more popular as a murder option. 
+
+This time it''s Target A and Target B. Both are supposed to be pretty skilled operators and they won''t be flying alone. 
+
+I''d make sure whoever you get to help you on this one can handle a fight.' WHERE id = 76;
+UPDATE contracts SET title = 'Simultaneous Release', description = 'Guess what Target A and Target B have in common? 
+
+You''re gonna kill ''em both!
+
+Difficult part is that I need you to kill them at the same time so that one doesn''t have a chance to escape, or worse, kill the person trying to kill them. 
+
+So put together a team to track one target while you handle the other. Divide and conquer!
+
+And despite the fact they made a mess big enough to get themselves killed, my understanding is that they are relative lightweights. Don''t expect you to meet too much resistance.' WHERE id = 77;
+UPDATE contracts SET title = 'Friends to the End', description = 'I really hope that you''re not buddies with Target A or Target B because I need you to ghost the pair of them... at the same time...
+
+Of course they wouldn''t make this easy on us and travel in the same ship or something obvious like that. No. You''re going to have to put together a crew so that you can down both targets simultaneously. 
+
+As soon as one of them is dead, be ready to drop the other one too. Can''t risk either of them getting away.
+
+And I figure I should mention that not only are both pretty good pilots, but that they''ll be traveling with backup too. Nothing you can''t handle I''m sure.' WHERE id = 78;
+UPDATE contracts SET title = 'Double Trouble', description = 'If you had asked me, I would''ve said Target A and Target B would have wound up eventually killing each other. But instead you''re going to get the honor of killing them both at the same time.
+
+You''re going to have to work for it, though.
+
+Each one is trouble. Serious records, highly trained, and too smart to fly without backup.
+
+Gather the best crew you can on this one. You''ll need it. And don''t be stingy. You can''t spend credits if you''re dead.' WHERE id = 79;
+UPDATE contracts SET title = 'Death do us Part', description = 'It''s pretty touching when you run across a pair that''s willing to die for one another. Stupid but touching.
+
+Target A and Target B chose to stand by each other and now its gone and got them dead.
+
+Find a friend of your own and hunt each of these two down. Make sure you kill them at the same time. Call it poetic justice.
+
+They should die pretty easy. Maybe they''ll have a ship or two with them but I don''t think it''ll be anything to worry about.' WHERE id = 80;
+UPDATE contracts SET title = 'Two for the Price of Two' WHERE id = 81;
